6dbd5d7194a243a0fca7d2a43f4c8c527b07cf04
josim-magnetism <josim@magnetismtech.com>
2025-12-02 10:25:05 +0600

    date change

--- resources/views/backend/purchase/create.blade.php
+++ resources/views/backend/purchase/create.blade.php
@@ -100,8 +100,10 @@
                             <div class="col-md-4">
                                 <div class="input-group mb-3">
                                     <span class="input-group-text" id="basic-addon1">Date</span>
-                                    <input type="date" name="date" id="" value="{{ old('date') }}"
-                                        class="form-control" placeholder="dd/mm/yyyy" required>
+                                    <input type="text" placeholder="dd/mm/yyyy" id="issue_date"
+       class="form-control" name="issue_date"
+       value="{{ date('d/m/Y') }}"
+       autocomplete="off" required>
                                 </div>
                             </div>
                             <div class="table-div">
